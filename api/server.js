e9d4ae9a0b646e65235d784d9a8ce08e0ef5d953
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2023-11-21 17:17:48 -0300

    fix: adicionando uma linha só p reiniciar o deploy

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(100000, () => {
+server.listen(50000, () => {
     console.log('JSON Server is running')
 })
 
