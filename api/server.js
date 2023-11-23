35e541fd40539189db06c7d6378400600efe5ee2
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2023-11-23 16:50:00 -0300

    fix: atualização da tabela herois

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(50000, () => {
+server.listen(85000, () => {
     console.log('JSON Server is running')
 })
 
